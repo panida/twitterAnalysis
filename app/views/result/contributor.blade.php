4e7ae3008e147994209a7b06d10ec4d888d7cdee
plkumjorn <plkumjorn@gmail.com>
2014-10-20 02:50:49 +0700

    - Add Interesting Contributors - Rearrange Contributors Page

--- app/views/result/contributor.blade.php
+++ app/views/result/contributor.blade.php
@@ -20,6 +20,88 @@
 	</ul>
 </div>
 
+<style>
+.panel{
+	width: 200px;
+}
+</style>
+
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="col-lg-9 col-lg-offset-2">
+			<h2 style="font-family:cursive;">Top Contributors</h2>
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-primary">
+				    <div class="panel-heading">
+				        <div class="row">
+				            <div class="col-xs-3">
+				                <a href="http://twitter.com/Prasong_lert" class="avatar" target="_blank"><img alt="Prasong_lert" src="http://pbs.twimg.com/profile_images/467959978716692481/fcU2IIey_normal.jpeg" /></a>
+				            </div>
+				            <div class="col-xs-9 text-right">
+				            	<div class="huge">32.4k</div>
+				                <div>impressions</div>
+				            </div>
+				        </div>
+				    </div>				 
+				    <a href="http://twitter.com/Prasong_lert">
+				        <div class="panel-footer">
+				            <span class="pull-left">@Prasong_lert</a></span>
+				            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				            <div class="clearfix"></div>
+				        </div>
+				    </a>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-green">
+				    <div class="panel-heading">
+				        <div class="row">
+				            <div class="col-xs-3">
+				                <a href="http://twitter.com/mmppz" class="avatar" target="_blank"><img alt="mmppz" src="http://pbs.twimg.com/profile_images/518719177662550016/83goccuU_normal.jpeg" /></a>
+				            </div>
+				            <div class="col-xs-9 text-right">
+				            	<div class="huge">12</div>
+				                <div>retweets</div>
+				            </div>
+				        </div>
+				    </div>
+				    <a href="http://twitter.com/mmppz">
+				        <div class="panel-footer">
+				            <span class="pull-left">@mmppz</a></span>
+				            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				            <div class="clearfix"></div>
+				        </div>
+				    </a>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-yellow">
+				    <div class="panel-heading">
+				        <div class="row">
+				            <div class="col-xs-3">
+				                <a href="http://twitter.com/Tlezwinyu" class="avatar" target="_blank"><img src="http://pbs.twimg.com/profile_images/470578977975709696/mW4CDI7S_normal.jpeg" alt="iiDudes" class="avatar"></a>
+				            </div>
+				            <div class="col-xs-9 text-right">
+				            	<div class="huge">2.7k</div>
+				                <div>Avg RTs</div>
+				            </div>
+				        </div>
+				    </div>
+				    <a href="http://twitter.com/Tlezwinyu">
+				        <div class="panel-footer">
+				            <span class="pull-left">@Tlezwinyu</a></span>
+				            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				            <div class="clearfix"></div>
+				        </div>
+				    </a>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="col-lg-4 col-lg-offset-2">
     <h2 style="font-family:cursive;">Contributors</h2>
     <div class="table-responsive">
@@ -339,77 +421,216 @@
 
   	</div>
 </div>
-<style>
-.panel{
-	width: 80%;
-}
-</style>
-<div class="col-lg-3">
-	<h2 style="font-family:cursive;">Top Contributors</h2>
-	<div class="panel panel-primary">
-	    <div class="panel-heading">
-	        <div class="row">
-	            <div class="col-xs-3">
-	                <a href="http://twitter.com/Prasong_lert" class="avatar" target="_blank"><img alt="Prasong_lert" src="http://pbs.twimg.com/profile_images/467959978716692481/fcU2IIey_normal.jpeg" /></a>
-	            </div>
-	            <div class="col-xs-9 text-right">
-	            	<div class="huge">32.4k</div>
-	                <div>impressions</div>
-	            </div>
-	        </div>
-	    </div>
-	    <a href="http://twitter.com/Prasong_lert">
-	        <div class="panel-footer">
-	            <span class="pull-left">@Prasong_lert</a></span>
-	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-	            <div class="clearfix"></div>
-	        </div>
-	    </a>
-	</div>
 
-	<div class="panel panel-green">
-	    <div class="panel-heading">
-	        <div class="row">
-	            <div class="col-xs-3">
-	                <a href="http://twitter.com/mmppz" class="avatar" target="_blank"><img alt="mmppz" src="http://pbs.twimg.com/profile_images/518719177662550016/83goccuU_normal.jpeg" /></a>
-	            </div>
-	            <div class="col-xs-9 text-right">
-	            	<div class="huge">12</div>
-	                <div>retweets</div>
-	            </div>
-	        </div>
-	    </div>
-	    <a href="http://twitter.com/mmppz">
-	        <div class="panel-footer">
-	            <span class="pull-left">@mmppz</a></span>
-	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-	            <div class="clearfix"></div>
-	        </div>
-	    </a>
-	</div>
+<div class="col-lg-4">
+  	<h2 style="font-family:cursive;">Interesting Contributors</h2>
+    <div class="table-responsive">
+    	
+	        <table class="table table-hover table-striped">
+		        <thead>
+		          <th>&nbsp;</th>
+		          <th>Tweets</th>
+		          <th>RTs</th>
+		          <th>Impressions</th>
+		        </thead>
+		        <tbody>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/thailand_jp">thailand_jp</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>8.5k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/JBGOT7_Thailand">JBGOT7_Thailand</a></td>
+		            <td>2</td>
+		            <td>0</td>
+		            <td>6.6k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/CollectedN">CollectedN</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>5.1k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/weyoyeyo">weyoyeyo</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>2.8k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/iTravel_Channel">iTravel_Channel</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>2.5k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/MINO_THAILAND">MINO_THAILAND</a></td>
+		            <td>4</td>
+		            <td>0</td>
+		            <td>2.3k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/unctadwif">unctadwif</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>2k</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/NEY_Sulli94">NEY_Sulli94</a></td>
+		            <td>4</td>
+		            <td>0</td>
+		            <td>628</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/9af0f531e5684db">9af0f531e5684db</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>603</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/ytrehcod">ytrehcod</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>557</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/PaPinn_z">PaPinn_z</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>490</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/JMinFa_BF">JMinFa_BF</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>484</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/asiapiamrak">asiapiamrak</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>441</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/JRMUKYOOK">JRMUKYOOK</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>381</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/pawind">pawind</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>370</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/aninafatini">aninafatini</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>313</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/pangram_felton">pangram_felton</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>312</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/Esterlify">Esterlify</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>267</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/YalindaL">YalindaL</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>255</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/theOther04">theOther04</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>252</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/ployypkt">ployypkt</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>251</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/NooLekMeePooH">NooLekMeePooH</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>238</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/xxludae">xxludae</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>219</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/viewrinrada">viewrinrada</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>197</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/bun_lucila">bun_lucila</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>153</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/Nnowzs">Nnowzs</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>149</td>
+		          </tr>
+		          
+		        <tr>
+		            <td class="screen_name"><a href="http://twitter.com/Jomjam_XH">Jomjam_XH</a></td>
+		            <td>1</td>
+		            <td>0</td>
+		            <td>143</td>
+		          </tr>
+		          
+	         		          
+		        </tbody>
+		      </table>
 
-	<div class="panel panel-yellow">
-	    <div class="panel-heading">
-	        <div class="row">
-	            <div class="col-xs-3">
-	                <a href="http://twitter.com/Tlezwinyu" class="avatar" target="_blank"><img alt="Tlezwinyu" src="http://pbs.twimg.com/profile_images/521026781399097344/Ho5jUndV_normal.jpeg" /></a>
-	            </div>
-	            <div class="col-xs-9 text-right">
-	            	<div class="huge">2.7k</div>
-	                <div>Avg RTs</div>
-	            </div>
-	        </div>
-	    </div>
-	    <a href="http://twitter.com/Tlezwinyu">
-	        <div class="panel-footer">
-	            <span class="pull-left">@Tlezwinyu</a></span>
-	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-	            <div class="clearfix"></div>
-	        </div>
-	    </a>
-	</div>
+  	</div>
+
 </div>
-
 
    
 
