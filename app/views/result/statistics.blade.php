ce852b11e05a1cbce48526bd1b5b6736f8b5e31b
plkumjorn <plkumjorn@gmail.com>
2014-12-17 21:48:57 +0700

    Initialize Tab System

--- app/views/result/statistics.blade.php
+++ app/views/result/statistics.blade.php
@@ -132,102 +132,97 @@ $(function () {
 @stop
 @section('TabContent')
 <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-	<ul class="nav nav-tabs" role="tablist">
-		<li class="active"><a href="{{URL::to('result/statistics')}}">Statistics</a></li>
-		<li><a href="{{URL::to('result/speedAndLifeCycle')}}">Speed and Life Cycle</a></li>
-		<li><a href="{{URL::to('result/contributor')}}">Contributor</a></li>
-		<li><a href="{{URL::to('result/tweetTimeline')}}">Tweet Timeline</a></li>
+	<ul id="tabMenu" class="nav nav-tabs" role="tablist">
+		<li class="active"><a href="#statistics" data-toggle="tab">Statistics</a></li>
+		<li><a href="#speedAndLifeCycle" data-toggle="tab">Speed and Life Cycle</a></li>
+		<li><a href="#contributor" data-toggle="tab">Contributor</a></li>
+		<li><a href="#tweetTimeline" data-toggle="tab">Tweet Timeline</a></li>
 	</ul>
 
-	<div id="page-wrapper">
 
-		<div class="container-fluid">
-
-			<div class="row">
-				<div class="col-lg-3">
-					<div style="text-align: right; font-family:cursive;"><h1>486</h1>posts</div>
-				</div> 
-				<div class="col-lg-3">
-					<div style="text-align: right; font-family:cursive;"><h1>1203</h1>users</div>
-				</div>
-				<div class="col-lg-3">
-					<div style="text-align: right; font-family:cursive;"><h1>32599</h1>impressions</div>
-				</div>
-			</div>
-			<!-- /.row -->
-			<br>
-			<div class="row">
-                <div class="col-lg-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Posts Proportion</h3>
-                        </div>
-                        <div class="panel-body">
-                            <div id="activity" style="height:350px"></div>
-                        </div>
+<div id="myTabContent" class="tab-content">
+   <div class="tab-pane fade in active" id="statistics">
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div style="text-align: right; font-family:cursive;"><h1>486</h1>posts</div>
+                    </div> 
+                    <div class="col-lg-3">
+                        <div style="text-align: right; font-family:cursive;"><h1>1203</h1>users</div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div style="text-align: right; font-family:cursive;"><h1>32599</h1>impressions</div>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Application Proportion</h3>
+                <!-- /.row -->
+                <br>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Posts Proportion</h3>
+                            </div>
+                            <div class="panel-body">
+                                <div id="activity" style="height:350px"></div>
+                            </div>
                         </div>
-                        <div class="panel-body">
-                            
-                             
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Application Proportion</h3>
+                            </div>
+                            <div class="panel-body">
                                 <div id="device" style="height:350px">
-                                    
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
                 <div class="row">
-
                     <div class="col-lg-12">
                         <div class="panel panel-red">
                             <div class="panel-heading">
                                 <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Top Retweeted Posts</h3>
                             </div>
                             <div class="panel-body">
-
                                 <ul class="chat">
                                     <li class="left clearfix">
-
                                         <span class="chat-img pull-left">
-                                          <a href="http://twitter.com/ipd69" class="tweet_avatar2">
-                                            <img src="http://pbs.twimg.com/profile_images/521337632169553921/A8IMmV6E_normal.jpeg" alt="ipd69" class="avatar">
-                                        </a>
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font"><a href="http://twitter.com/ipd69" class="tweet_screen_name2 screen_name">พี่ดี้ลำเอียง</a></strong> 
-                                            <span style="color:#AAAAAA;">@ipd69</span>
-                                            <small class="pull-right text-muted">
-                                                <i class="fa fa-clock-o fa-fw"></i> 4 months ago
+                                            <a href="http://twitter.com/ipd69" class="tweet_avatar2">
+                                                <img src="http://pbs.twimg.com/profile_images/521337632169553921/A8IMmV6E_normal.jpeg" alt="ipd69" class="avatar">
+                                            </a>
+                                        </span>
+                                        <div class="chat-body clearfix">
+                                            <div class="header">
+                                                <strong class="primary-font"><a href="http://twitter.com/ipd69" class="tweet_screen_name2 screen_name">พี่ดี้ลำเอียง</a></strong> 
+                                                <span style="color:#AAAAAA;">@ipd69</span>
+                                                <small class="pull-right text-muted">
+                                                    <i class="fa fa-clock-o fa-fw"></i> 4 months ago
+                                                </small>
+                                            </div>
+                                            <p>
+                                                กูไม่เคยเห็นการทำ "รัฐประหาร" แบบนี้ในโลกนี้! #นี่กูพูดจริงๆ  <a href="http://t.co/qu0RcBP7IJ" rel="nofollow">http://t.co/qu0RcBP7IJ</a>
+                                            </p>
+                                            <small class="text-muted">
+                                                <i class="fa fa-retweet fa-fw"></i> 10 retweets
                                             </small>
                                         </div>
-                                        <p>
-                                            กูไม่เคยเห็นการทำ "รัฐประหาร" แบบนี้ในโลกนี้! #นี่กูพูดจริงๆ  <a href="http://t.co/qu0RcBP7IJ" rel="nofollow">http://t.co/qu0RcBP7IJ</a>
-                                        </p>
-                                        <small class="text-muted">
-                                            <i class="fa fa-retweet fa-fw"></i> 10 retweets
-                                        </small>
-                                    </div>
-                                </li>
-                                <li class="left clearfix">
-                                    <span class="chat-img pull-left">
-                                        <a href="http://twitter.com/Khunpone" class="tweet_avatar2">
-                                            <img src="http://pbs.twimg.com/profile_images/948274723/as015069_normal.jpg" alt="Khunpone" class="avatar">
-                                        </a>
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font"><a href="http://twitter.com/Khunpone" class="tweet_screen_name2 screen_name">Teerapong Ponesiri</a></strong>
-                                            <span style="color:#AAAAAA;">@Khunpone</span>
-                                            <small class="pull-right text-muted">
-                                                <i class="fa fa-clock-o fa-fw"></i> about 9 hours ago</small>
+                                    </li>
+                                    <li class="left clearfix">
+                                        <span class="chat-img pull-left">
+                                            <a href="http://twitter.com/Khunpone" class="tweet_avatar2">
+                                                <img src="http://pbs.twimg.com/profile_images/948274723/as015069_normal.jpg" alt="Khunpone" class="avatar">
+                                            </a>
+                                        </span>
+                                        <div class="chat-body clearfix">
+                                            <div class="header">
+                                                <strong class="primary-font"><a href="http://twitter.com/Khunpone" class="tweet_screen_name2 screen_name">Teerapong Ponesiri</a></strong>
+                                                <span style="color:#AAAAAA;">@Khunpone</span>
+                                                <small class="pull-right text-muted">
+                                                    <i class="fa fa-clock-o fa-fw"></i> about 9 hours ago
+                                                </small>
                                             </div>
                                             <p>
                                                 สื่อเยอรมันเรียกลุงว่าผู้นำรัฐประหาร EUไม่ยอมจัดประชุมทวิภาคีด้วย สรุป รัฐประหารไทยทำเศรษฐกิจทรุด และจะเลวร้ายลงอีก ..หลอกกันได้ไม่นานหรอก
@@ -249,38 +244,71 @@ $(function () {
                                                 <strong class="primary-font"> <a href="http://twitter.com/iiDudes" class="tweet_screen_name2 screen_name">นป.นักดมกาวในตำนาน</a></strong>
                                                 <span style="color:#AAAAAA;">@iiDudes</span>
                                                 <small class="pull-right text-muted">
-                                                    <i class="fa fa-clock-o fa-fw"></i> about 9 hours ago</small>
-                                                </div>
-                                                <p>
-                                                    เค้าไม่ได้รังเกียจไทย
-
-                                                    แงงงงงงงงง
-
-                                                    เค้าเกลียดการยึดอำนาจ เค้าเกลียดการรัฐประหาร เค้าเกลียดเผด็จการ แงงงงงง
-                                                </p>
-                                                <small class="text-muted">
-                                                    <i class="fa fa-retweet fa-fw"></i> 7 retweets
+                                                    <i class="fa fa-clock-o fa-fw"></i> about 9 hours ago
                                                 </small>
                                             </div>
-                                        </li>
+                                            <p>
+                                                เค้าไม่ได้รังเกียจไทย
 
-                                    </ul>
-                                    <!-- /.panel .chat-panel -->
-                                    <div class="text-right">
-                                        <a href="#">View Details <i class="fa fa-arrow-circle-right"></i></a>
-                                    </div>
+                                                แงงงงงงงงง
+
+                                                เค้าเกลียดการยึดอำนาจ เค้าเกลียดการรัฐประหาร เค้าเกลียดเผด็จการ แงงงงงง
+                                            </p>
+                                            <small class="text-muted">
+                                                <i class="fa fa-retweet fa-fw"></i> 7 retweets
+                                            </small>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <!-- /.panel .chat-panel -->
+                                <div class="text-right">
+                                    <a href="#">View Details <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                         </div>
-
                     </div>
-
-
-
                 </div>
-                <!-- /.container-fluid -->
-
             </div>
-            <!-- /#page-wrapper -->
+            <!-- /.container-fluid -->
         </div>
+        <!-- /#page-wrapper -->
+   </div>
+   <div class="tab-pane fade" id="speedAndLifeCycle">
+      <p>iOS is a mobile operating system developed and distributed by Apple 
+         Inc. Originally released in 2007 for the iPhone, iPod Touch, and 
+         Apple TV. iOS is derived from OS X, with which it shares the 
+         Darwin foundation. iOS is Apple's mobile version of the 
+         OS X operating system used on Apple computers.</p>
+   </div>
+   <div class="tab-pane fade" id="contributor">
+      <p>jMeter is an Open Source testing software. It is 100% pure 
+      Java application for load and performance testing.</p>
+   </div>
+   <div class="tab-pane fade" id="tweetTimeline">
+      <p>Enterprise Java Beans (EJB) is a development architecture 
+         for building highly scalable and robust enterprise level    
+         applications to be deployed on J2EE compliant 
+         Application Server such as JBOSS, Web Logic etc.
+      </p>
+   </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         @stop
