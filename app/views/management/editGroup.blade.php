867573e041bf409140b446e40b00afe8816327c2
Panida <cp.panida@gmailcom>
2015-01-03 20:55:20 +0700

    group management except add member

--- app/views/management/editGroup.blade.php
+++ app/views/management/editGroup.blade.php
@@ -3,11 +3,23 @@
 	<br>
     <h2 class="panel-title onlythaibold" style="font-size:25px;"><i class="fa fa-fw fa-folder-open" style="color:#EBE241;"></i> แก้ไขกลุ่มตัวอย่าง</h2>
     <br>
-    <form class="form-horizontal" role="form" method="POST" action="{{{ URL::to('editgroup') }}}" accept-charset="UTF-8">
+    @if (Session::get('error'))
+        <div class="alert alert-danger">
+        @if (is_array(Session::get('error')))
+            {{ head(Session::get('error')) }}
+        @else
+            {{{ Session::get('error') }}}
+        @endif
+        </div>
+    @endif
+    @if (Session::get('notice'))
+        <div class="alert alert-success">{{ Session::get('notice') }}</div>
+    @endif
+    {{Form::open(array('url' => '/group/'.$groupDetail->groupid,'method'=>'post','class' => 'form-horizontal','accept-charset'=>'UTF-8'))}}    
         <div class="form-group">
             <label for="name" class="col-sm-3 control-label">ชื่อกลุ่ม <span class="required">*</span></label>
             <div class="col-sm-5">
-                {{ Form::text('name', 'คนดัง', ['class' => 'form-control', 'placeholder' => 'คนดัง', 'required' => 'required']) }}
+                {{ Form::text('name', $groupDetail->groupname, ['class' => 'form-control', 'placeholder' => 'ชื่อกลุ่ม', 'required' => 'required']) }}
                 <label class="control-label" style="color:darkred; font-size:12px;">&nbsp; หมายเหตุ: ชื่อกลุ่มต้องไม่ซ้ำกับกลุ่มอื่นๆที่มีอยู่ก่อนหน้า</label>
             </div>     
             
@@ -16,20 +28,20 @@
         <div class="form-group">
             <label for="description" class="col-sm-3 control-label">รายละเอียดของกลุ่ม </label>
             <div class="col-sm-8">
-                {{ Form::textarea('description', 'นักคิด นักวิชาการ ดารา', ['class' => 'form-control', 'placeholder' => 'รายละเอียดของกลุ่ม', 'cols' => '30', 'rows' => '3']) }}
+                {{ Form::textarea('description', $groupDetail->description, ['class' => 'form-control', 'placeholder' => 'รายละเอียดของกลุ่ม', 'cols' => '30', 'rows' => '3']) }}
             </div>
         </div><!--form-group-->
         <div class="form-group">
             <div class="col-sm-offset-3 col-sm-8">
-                <button type="submit" class="btn btn-primary">บันทึกการแก้ไข</button>
+                {{ Form::submit('บันทึกการแก้ไข', array('class'=>'btn btn-primary'))}}
                 &nbsp;
-                <button type="button" class="btn btn-danger" onclick="confirmDeleteGroup()">ลบกลุ่มตัวอย่างนี้</button>
+                <a href="{{{ URL::to('deleteGroup/'.$groupDetail->groupid) }}}"><button type="button" class="btn btn-danger" onclick="confirmDeleteGroup()">ลบกลุ่มตัวอย่างนี้</button></a>
             </div>
         </div>
     </form>
 
     <br>
-    <h2 class="panel-title onlythaibold" style="font-size:25px;"><i class="fa fa-fw fa-user" style="color:#4171EB;"></i> จัดการสมาชิกในกลุ่ม - จำนวนสมาชิกปัจจุบัน: 5 คน</h2>
+    <h2 class="panel-title onlythaibold" style="font-size:25px;"><i class="fa fa-fw fa-user" style="color:#4171EB;"></i> จัดการสมาชิกในกลุ่ม - จำนวนสมาชิกปัจจุบัน: {{$members->count()}} คน</h2>
     <br>
     <form class="form-horizontal" role="form" method="POST" action="{{{ URL::to('addmember') }}}" accept-charset="UTF-8">
         <div class="form-group">
@@ -37,7 +49,7 @@
             <div class="col-sm-4">
                 <div class="input-group">
                     <span class="input-group-addon">@</span>
-                    {{ Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'username', 'required' => 'required']) }}       
+                    {{ Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'screen name', 'required' => 'required']) }}       
                 </div>
             </div>        
             <div class="col-sm-3">
@@ -50,136 +62,38 @@
             <div class="panel-heading">
                 <h3 class="panel-title thaibold" style="font-size:20px;">
                     <i class="fa fa-long-arrow-right"></i> 
-                    สมาชิกในกลุ่มคนดังทั้งหมด
+                    สมาชิกในกลุ่ม{{$groupDetail->groupname}}ทั้งหมด
 <!--                     <span class="glyphicon glyphicon-info-sign" style="font-size:15px;" aria-hidden="true" title="ในกรณีของรีทวีต จำนวน Follower ที่ใช้เรียงจะเป็นของผู้รีทวีต"></span> -->
                 </h3>
             </div>
             <div class="panel-body" style="max-height: 500px; overflow-y: scroll;">
-                <ul class="chat">                    
+                <ul class="chat">    
+                    @foreach($members as $member)                
                     <li class="left clearfix">
                         <span class="chat-img pull-left">
-                            <a href="http://twitter.com/Keztii_PinkyRox" target="blank" class="tweet_avatar2">
-                                <img src="http://pbs.twimg.com/profile_images/378800000107726833/41e20a29a69daca86ee99c62675b5ef2_normal.jpeg" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
+                            <a href= "{{$member->user_timeline_url}}" target="blank" class="tweet_avatar2">
+                                <img src="{{$member->profile_pic_url}}" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
                             </a>
                         </span>
                         <div class="chat-body clearfix">
                             <div class="header">
-                                <strong class="primary-font"><a href="http://twitter.com/Keztii_PinkyRox" target="blank" class="tweet_screen_name2 screen_name">♡ K-A-T-E ♡</a></strong> 
-                                <span style="color:#AAAAAA;">@Keztii_PinkyRox</span>    
+                                <strong class="primary-font"><a href="{{$member->user_timeline_url}}" target="blank" class="tweet_screen_name2 screen_name">{{$member->name}}</a></strong> 
+                                <span style="color:#AAAAAA;">{{'@'.$member->screenname}}</span>    
                                 <span class="chat-img pull-right">                                    
-                                    <a href="{{{ URL::to('deleteMember') }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
+                                    <a href="{{{ URL::to('deleteMember/'.$groupDetail->groupid.'/'.$member->userkey) }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
                                 </span>                            
                             </div>
                             <p>
-                                Faculty of Law ABAC ,, My bro: @WayoTS6 @Icemamkak My sis: @bell_belt I ♥ #TheStar6 @MrSingtoTheStar @Obojama @s8shane Harry Potter
+                                {{$member->description}}
                             </p>
-                            <small class="text-muted">
+                            <!-- <small class="text-muted">
                                 <i class="fa fa-comment fa-fw"></i> 472 tweets
                                 <i class="fa fa-user fa-fw"></i> 62 followers
                                 <i class="fa fa-user fa-fw"></i> 113 following
-                            </small>
-                        </div>
-                    </li>  
-
-                    <li class="left clearfix">
-                        <span class="chat-img pull-left">
-                            <a href="http://twitter.com/zeza136" target="blank" class="tweet_avatar2">
-                                <img src="http://pbs.twimg.com/profile_images/505583292586803200/YntDUqus_normal.jpeg" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
-                            </a>
-                        </span>
-                        <div class="chat-body clearfix">
-                            <div class="header">
-                                <strong class="primary-font"><a href="http://twitter.com/zeza136" target="blank" class="tweet_screen_name2 screen_name">.</a></strong> 
-                                <span style="color:#AAAAAA;">@zeza136</span>    
-                                <span class="chat-img pull-right">                                    
-                                    <a href="{{{ URL::to('deleteMember') }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
-                                </span>                            
-                            </div>
-                            <p>
-                                Thailand Demo-cracy ไม่เชื่อในคนดี คนดีอย่ากลัวการถูกตรวจสอบ
-                            </p>
-                            <small class="text-muted">
-                                <i class="fa fa-comment fa-fw"></i> 472 tweets
-                                <i class="fa fa-user fa-fw"></i> 62 followers
-                                <i class="fa fa-user fa-fw"></i> 113 following
-                            </small>
+                            </small> -->
                         </div>
                     </li> 
-
-                    <li class="left clearfix">
-                        <span class="chat-img pull-left">
-                            <a href="http://twitter.com/iiploii" target="blank" class="tweet_avatar2">
-                                <img src="http://pbs.twimg.com/profile_images/539117249580109824/EfTOYDfN_normal.jpeg" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
-                            </a>
-                        </span>
-                        <div class="chat-body clearfix">
-                            <div class="header">
-                                <strong class="primary-font"><a href="http://twitter.com/iiploii" target="blank" class="tweet_screen_name2 screen_name">฿ถุงน่องหิวเงิน฿</a></strong> 
-                                <span style="color:#AAAAAA;">@iiploii</span>    
-                                <span class="chat-img pull-right">                                    
-                                    <a href="{{{ URL::to('deleteMember') }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
-                                </span>                            
-                            </div>
-                            <p>
-                                อยากได้ทุนไปเรียนเมืองนอก แต่โง่ / Lier, lier, pants on fire  / soy estudiante de español / รับแปลงาน TH-ENG นะ / I'm #ThoMinho
-                            </p>
-                            <small class="text-muted">
-                                <i class="fa fa-comment fa-fw"></i> 472 tweets
-                                <i class="fa fa-user fa-fw"></i> 62 followers
-                                <i class="fa fa-user fa-fw"></i> 113 following
-                            </small>
-                        </div>
-                    </li> 
-
-                    <li class="left clearfix">
-                        <span class="chat-img pull-left">
-                            <a href="http://twitter.com/paint_PMP" target="blank" class="tweet_avatar2">
-                                <img src="http://pbs.twimg.com/profile_images/528120071164416001/ocdVT9pB_normal.jpeg" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
-                            </a>
-                        </span>
-                        <div class="chat-body clearfix">
-                            <div class="header">
-                                <strong class="primary-font"><a href="http://twitter.com/paint_PMP" target="blank" class="tweet_screen_name2 screen_name">Paintttt.</a></strong> 
-                                <span style="color:#AAAAAA;">@paint_PMP</span>    
-                                <span class="chat-img pull-right">                                    
-                                    <a href="{{{ URL::to('deleteMember') }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
-                                </span>                            
-                            </div>
-                            <p>
-                                
-                            </p>
-                            <small class="text-muted">
-                                <i class="fa fa-comment fa-fw"></i> 472 tweets
-                                <i class="fa fa-user fa-fw"></i> 62 followers
-                                <i class="fa fa-user fa-fw"></i> 113 following
-                            </small>
-                        </div>
-                    </li> 
-
-                    <li class="left clearfix">
-                        <span class="chat-img pull-left">
-                            <a href="http://twitter.com/chatchamon" target="blank" class="tweet_avatar2">
-                                <img src="http://pbs.twimg.com/profile_images/482577002927366144/xUB3iuIF_normal.jpeg" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
-                            </a>
-                        </span>
-                        <div class="chat-body clearfix">
-                            <div class="header">
-                                <strong class="primary-font"><a href="http://twitter.com/chatchamon" target="blank" class="tweet_screen_name2 screen_name">Chatchamon Writer</a></strong> 
-                                <span style="color:#AAAAAA;">@chatchamon</span>    
-                                <span class="chat-img pull-right">                                    
-                                    <a href="{{{ URL::to('deleteMember') }}}"><button type="button" class="btn btn-danger" onclick="confirmDelete()">ลบออกจากกลุ่ม</button></a>
-                                </span>                            
-                            </div>
-                            <p>
-                                ชัชชมนต์ นักเขียนแจ่มใสค่ะ
-                            </p>
-                            <small class="text-muted">
-                                <i class="fa fa-comment fa-fw"></i> 472 tweets
-                                <i class="fa fa-user fa-fw"></i> 62 followers
-                                <i class="fa fa-user fa-fw"></i> 113 following
-                            </small>
-                        </div>
-                    </li>                             
+                    @endforeach                            
                 </ul>
                 <!-- /.panel .chat-panel -->                                
             </div>
