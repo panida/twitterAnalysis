5d36fac554afcb6a5137917028033b0a4a4649c0
Panida <cp.panida@gmail.com>
2015-01-11 21:10:34 +0700

    fix zoom scale when change day, month, week scale add color when select button

--- app/models/FolloweeScript.php
+++ app/models/FolloweeScript.php
@@ -52,27 +52,4 @@ class TwitterAPIHelper
 		}
 		return $result;
 	}
-
-	public static function find_followee($userkey,$screenname,$cursor){
-		$tmhOAuth = new tmhOAuth(array( //SentimentalAnalysisPLJ
-			'consumer_key'    => '0LRwCUJKeY7PRmwLsrPYRh6Cm',
-			'consumer_secret' => 'AMrh3UX7C5voB1rXHbsAIm1VtbUi5CDDvgS4jTUCvlDwS4cMEi',
-			'user_token'      => '2578760185-31Fqd6x9DeXfR63n6HsrAAR2Bkrho86ROSZp76V',
-			'user_secret'     => '9n2upQxbaZ5gRhKkOoJFghBdqJVW2KHfIKyZdqIviRLQZ',
-		));
-		$method = 'https://api.twitter.com/1.1/friends/ids.json';
-		$params = array(
-			'screen_name'     => $screenName,
-			'cursor'		  => intval($cursor),
-			'stringify_ids'	  => true
-	
-		);
-		$code = $tmhOAuth->request('GET', $method, $params);
-		$result = [];
-		if($code == 200){
-			//$result = $tmhOAuth;
-			$result = json_decode($tmhOAuth->response["response"], true);
-		}
-		return $result;
-	}
 }
