0449b79c5f8026fd0434ca45da13930317d0daae
Panida <cp.panida@gmail.com>
2015-01-16 18:00:48 +0700

    add followeeProcessedUser Table to check if added member process

--- app/views/management/addGroup.blade.php
+++ app/views/management/addGroup.blade.php
@@ -37,4 +37,49 @@
             {{ Form::submit('บันทึก', array('class'=>'btn btn-success'))}}
         </div>
     </div>
+
+    <br>
+    <h2 class="panel-title onlythaibold" style="font-size:25px;"><i class="fa fa-fw fa-user" style="color:#FF0000;"></i> รายการสมาชิกที่ยังประมวลผลไม่เสร็จสิ้น: {{$notFinishedMembers->count()}} คน</h2>
+    <br>
+    @if($notFinishedMembers->count()!=0)
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="panel panel-primary">
+            
+            <div class="panel-body" style="max-height: 500px; overflow-y: scroll;">
+                <ul class="chat">    
+                    @foreach($notFinishedMembers as $member)                
+                    <li class="left clearfix">
+                        <span class="chat-img pull-left">
+                            <a href= "{{$member->user->user_timeline_url}}" target="blank" class="tweet_avatar2">
+                                <img src="{{$member->user->profile_pic_url}}" alt="ipd69" class="avatar" onerror="if (this.src != 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png') this.src = 'http://a0.twimg.com/sticky/default_profile_images/default_profile_1_normal.png';">
+                            </a>
+                        </span>
+                        <div class="chat-body clearfix">
+                            <div class="header">
+                                <strong class="primary-font"><a href="{{$member->user_timeline_url}}" target="blank" class="tweet_screen_name2 screen_name">{{$member->user->name}}</a></strong> 
+                                <span style="color:#AAAAAA;">{{'@'.$member->user->screenname}}</span>                                
+                            </div>
+                            <p>
+                                {{$member->user->description}}
+                            </p>
+                            <!-- <small class="text-muted">
+                                <i class="fa fa-comment fa-fw"></i> 472 tweets
+                                <i class="fa fa-user fa-fw"></i> 62 followers
+                                <i class="fa fa-user fa-fw"></i> 113 following
+                            </small> -->
+                        </div>
+                    </li> 
+                    @endforeach                            
+                </ul>
+                <!-- /.panel .chat-panel -->                                
+            </div>
+
+        </div>
+    </div>
+    @else
+    <div class="col-lg-11 col-md-11 col-sm-11 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+        <h5>ไม่มีสมาชิกที่อยู่ระหว่างประมวลผล</h5>
+    </div>
+    @endif
+
 @stop
