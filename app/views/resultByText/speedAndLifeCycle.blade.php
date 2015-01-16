9609d5f01d76dc069ec0bdf604f2bd87280bde85
Panida <cp.panida@gmail.com>
2015-01-16 10:39:35 +0700

    change style of date picker range

--- app/views/resultByText/speedAndLifeCycle.blade.php
+++ app/views/resultByText/speedAndLifeCycle.blade.php
@@ -8,7 +8,7 @@
 			<div class="panel-body">
 				<!-- <div><label for="start">Start date:</label><input id="start" style="width: 200px" value="10/10/2011" /><label for="end">End date:</label><input id="end" style="width: 200px" value="10/10/2012"/></div> -->
 				
-				<div>
+				<div style="position:relative">
 					<div class="row" style="height:40px"><div class="graphName"><h4>กิจกรรมทั้งหมด</h4></div></div>
 					<div class="row">
 						<div style="margin-left: 117px;display: inline">Scale</div>
@@ -21,11 +21,11 @@
 					</div>
 					<div id="allActivityGraph" style="margin: 0px 50px 20px 50px;"></div>
 					<input type="text" class = "datepickerGraphStartdate" 
-	                		style = 'left:778px; top:-560px; position:relative; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+	                		style = 'left:778px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
 	                <input type="text" class = "datepickerGraphEnddate" 
-	                		style = 'left:811px; top:-560px; position:relative; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+	                		style = 'left:906px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
 				</div>
-				<div>
+				<div style="position:relative">
 					<div class="row" style="height:40px"><div class="graphName"><h4>ประเภทของการทวีต</h4></div></div>
 					<div class="row">
 						<div style="margin-left: 117px;display: inline">Scale</div>
@@ -38,11 +38,11 @@
 					</div>
 					<div id="eachActivityGraph" style="margin:0px 50px 10px 50px;"></div>
 					<input type="text" class = "datepickerGraphStartdate" 
-	                		style = 'left:778px; top:-550px; position:relative; float:left;  width: 95px; height:22px; font-size:12px;font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+	                		style = 'left:778px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px;font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
 	                <input type="text" class = "datepickerGraphEnddate" 
-	                		style = 'left:811px; top:-550px; position:relative; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+	                		style = 'left:906px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
 	                </div>
-				<div>
+				<div style="position:relative">
 					 <div class="row" style="height:40px"><div class="graphName"><h4>ประเภทของแอพพลิเคชั่น</h4></div></div>
 					<div class="row">
 						<div style="margin-left: 117px;display: inline">Scale</div>
@@ -55,9 +55,9 @@
 					</div>
 					<div id="applicationGraph" style="margin:0px 50px 10px 50px;"></div>
 					<input type="text" class = "datepickerGraphStartdate" 
-                			style = 'left:778px; top:-550px; position:relative; float:left;  width: 95px; height:22px; font-size:12px;font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+                			style = 'left:778px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px;font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
                 	<input type="text" class = "datepickerGraphEnddate" 
-                			style = 'left:811px; top:-550px; position:relative; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
+                			style = 'left:906px; top:72px; position:absolute; float:left;  width: 95px; height:22px; font-size:12px; font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;' />
                 </div>
 			</div>
 		</div>
